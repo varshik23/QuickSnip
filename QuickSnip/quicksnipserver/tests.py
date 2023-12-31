91d96ec261610e772df56583bf00dfefb09dea6b
varshik23 <sontivarshik@gmail.com>
2023-12-31 16:12:14 -0700

    Updated the build configuartion

--- QuickSnip/quicksnipserver/tests.py
+++ QuickSnip/quicksnipserver/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# from django.urls import reverse
+# from rest_framework import status
+# from rest_framework.test import APITestCase
+# from .models import urls, clicks
+# from django.contrib.auth.models import User
+class Test(TestCase):
+    def test_is_true(self):
+        self.assertEqual(True, True)
